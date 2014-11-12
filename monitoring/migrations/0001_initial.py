9e644dea293470e2ab15c4dfbe27621852ca9929
Juan Jose <localsprout@ip-10-144-242-81.ec2.internal>
2014-11-12 03:15:54 +0000

    updating migrations to django 1.7

---
+++ monitoring/migrations/0001_initial.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=75)),
+                ('start_date', models.DateTimeField(verbose_name=b'datetime started')),
+                ('end_date', models.DateTimeField(null=True, verbose_name=b'datetime terminated')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Contact',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('phone', models.CharField(max_length=10, null=True)),
+                ('account', models.ForeignKey(to='monitoring.Account')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('country', models.CharField(max_length=100)),
+                ('state', models.CharField(max_length=100, null=True)),
+                ('city', models.CharField(max_length=100)),
+                ('street_address', models.CharField(max_length=175)),
+                ('zip_code', models.CharField(max_length=10, null=True)),
+                ('description', models.CharField(max_length=200, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Reading',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('type', models.CharField(max_length=20)),
+                ('data', models.CharField(max_length=200)),
+                ('date', models.DateTimeField(verbose_name=b'datetime received')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SensorGroup',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('description', models.CharField(max_length=250, null=True)),
+                ('is_active', models.BooleanField(default=True)),
+                ('account', models.ForeignKey(to='monitoring.Account')),
+                ('location', models.ForeignKey(to='monitoring.Location')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='reading',
+            name='sensor_group',
+            field=models.ForeignKey(to='monitoring.SensorGroup'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='location',
+            field=models.ForeignKey(to='monitoring.Location', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='user',
+            field=models.OneToOneField(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+    ]
